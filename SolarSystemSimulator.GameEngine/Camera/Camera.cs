b0e7a9f2cb16be205ec057ed905ebdab2907c14f
RononDex <tinoheuberger@hotmail.com>
2016-10-20 13:38:54 +0200

    Initial upload

---
+++ SolarSystemSimulator.GameEngine/Camera/Camera.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Camera
+{
+    public abstract class Camera : GameComponent
+    {
+        /// <summary>
+        /// The view matrix of the camera
+        /// </summary>
+        public Matrix ViewMatrix { get; set; }
+
+        /// <summary>
+        /// The position of the camera in the 3d world
+        /// </summary>
+        public Vector3 Position { get; set; }
+
+        /// <summary>
+        /// The direction in which the camera looks
+        /// </summary>
+        protected Vector3 Target { get; set; }
+
+        /// <summary>
+        /// The up vector of the camera for the rotation
+        /// </summary>
+        protected Vector3 UpVector { get; set; }
+
+        /// <summary>
+        /// Recalculates the view Matrix
+        /// </summary>
+        protected virtual void RecalcView()
+        {
+            this.ViewMatrix = Matrix.CreateLookAt(this.Position, this.Target, this.UpVector);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="engine">Reference to the game engine</param>
+        public Camera(GameEngine engine, Vector3 position = new Vector3(), Vector3 target = new Vector3(), Vector3 up = new Vector3()) : base(engine)
+        {
+            this.Position = position;
+            this.Target = target;
+            this.UpVector = up;
+            RecalcView();
+        }
+    }
+}
---
+++ SolarSystemSimulator.GameEngine/GameEngine.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    /// <summary>
+    /// The game engine for the simulation
+    /// Controls all the rendering
+    /// </summary>
+    public class GameEngine : Game
+    {
+        /// <summary>
+        /// All objects that are beeing rendered ingame are in this collection
+        /// </summary>
+        public List<Model> Objects { get; set; }
+
+        public View CurrentView { get; private set; }
+
+        /// <summary>
+        /// Updates the applications rendering
+        /// </summary>
+        /// <param name="gameTime"></param>
+        protected override void Draw(GameTime gameTime)
+        {
+
+        }
+    }
+}
